10556b76f451add71c7331f70e09fafbcfdb756e
Nuggets219 <tysonthomas207@gmail.com>
2024-01-21 18:14:57 -0700

    Update mods.py

--- mods.py
+++ mods.py
@@ -1,5 +1,5 @@
 ###REMOVE NUGGETS IF YOU WANT.
-mods = ["bezelea","imgeiser","jakecreatesstuff","lcolonq","nuggets219"]
+mods = [""]
 
 ####YOU HAVE TO REPLACE THIS WITH THE BROADCASTER AND ANY OTHER EXTREMELY HIGHLY TRUSTED PEOPLE.
-superTrusted = ""
+superTrusted = [""]
